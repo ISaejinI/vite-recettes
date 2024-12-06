121b98e21b79be6889f8bb6e87b19cd25063d7d1
Lou-Anne <saejinmx@gmail.com>
2024-12-06 11:20:40 +0100

    Récupération des données

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -1,16 +1,35 @@
-export default function headFunc() {
+export default function headFunc(fileJson) {
     return {
         isActive: false,
         iconName: 'menu',
-        totRecipes: 50,
+        totRecipes: 0,
+        recipes: [],
 
-        toggleMenu(){
+        //NOTE - Récupére les données à l'initialisation d'Alpine
+        async init() {
+            this.getRecipes(fileJson);
+        },
+
+        //NOTE - Affiche ou masquage du menu burger
+        toggleMenu() {
             this.isActive =! this.isActive;
             this.iconName = this.isActive?'menu-alt-right':'menu';
         },
 
-        recipesCount(){
-            return this.totRecipes;
+        //NOTE - Récupération des données
+        getRecipes(fileData) {
+            fetch(fileData)
+            .then(response => response.json())
+            .then(datas => {
+                this.setData(datas);
+            })
+        },
+
+        //NOTE - Insertion des données dans la variable
+        setData(datas) {
+            this.recipes = datas.recipes;
+            this.totRecipes = this.recipes.length;
+            console.log(datas.recipes);
         }
     }
 }
